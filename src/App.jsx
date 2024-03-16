a3b634a02026bc11949af27e09f34d4dc990dfaf
arvindydv <arvindydv03@gmail.com>
2024-03-16 14:57:23 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      {/* <h1>Quiz App</h1> */}
+      <Home />
+    </>
+  );
+}
+
+export default App;
